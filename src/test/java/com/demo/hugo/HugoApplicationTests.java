f6805e67d540493304d7770bd435f8bfa3da5e0d
lucasveron <veron_lucas@hotmail.com>
2017-07-01 18:36:54 -0300

    First commit

---
+++ src/test/java/com/demo/hugo/HugoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.hugo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HugoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
